0a3119af2aa669953336bf982a9876349e815c33
Jakub Zaba <kuba.zaba@gmail.com>
2016-02-10 23:46:21 +0100

    Create newsletters command

---
+++ app/console/CreateNewslettersCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console;
+
+use App\Model\UserModel;
+use App\Model\UserNewsletterModel;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class CreateNewslettersCommand extends Command
+{
+	protected function configure()
+	{
+		$this->setName('newsletters:create')
+			->setDescription('Create newsletters')
+			->addArgument(
+				'newsletterId',
+				InputArgument::REQUIRED,
+				'newsletter template id'
+			);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$newsletterId = (int)$input->getArgument('newsletterId');
+
+		/** @var UserNewsletterModel $userNewsletterModel */
+		$userNewsletterModel = $this->getHelper('container')->getByType(UserNewsletterModel::class);
+		/** @var UserModel $userModel */
+		$userModel = $this->getHelper('container')->getByType(UserModel::class);
+
+		$users = $userModel->getAll()->fetchAll();
+		foreach($users as $user) {
+			$userNewsletterModel->createNewsletter($user->id, $newsletterId);
+		}
+
+		$output->writeLn('Newsletters created');
+		return 0;
+	}
+}
--- app/model/UserNewsletterModel.php
+++ app/model/UserNewsletterModel.php
@@ -2,16 +2,15 @@
 
 namespace App\Model;
 
-
-use Nette\Database\Table\IRow;
 use Nette\Utils\Random;
+
 
 class UserNewsletterModel extends BaseModel
 {
 	const TABLE_NAME = 'users_newsletters';
 
 
-	public function createNewsletter(int $userId, int $newsletterId) : IRow
+	public function createNewsletter(int $userId, int $newsletterId)
 	{
 		return $this->insert([
 			'user_id' => $userId,
