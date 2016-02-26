5c9143609e7026b5e036bba6f433f4ce8c552348
Jakub Zaba <kuba.zaba@gmail.com>
2016-02-26 15:51:15 +0100

    DebugMode for dev subdomains

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->setDebugMode(in_array($_SERVER['HTTP_HOST'], ['kuba.eventigo.cz', 'filip.eventigo.cz']));
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
