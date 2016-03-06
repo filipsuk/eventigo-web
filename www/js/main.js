6885d26ee8bb4270c4309a41e7314fd80494c56e
Jakub Zaba <kuba.zaba@gmail.com>
2016-03-06 20:55:47 +0100

    Smooth scroll

--- www/js/main.js
+++ www/js/main.js
@@ -30,4 +30,18 @@ $(function () {
         }
     });
 
+
+    // Smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 300);
+                return false;
+            }
+        }
+    });
 });
