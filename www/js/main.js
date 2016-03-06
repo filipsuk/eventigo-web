8ce202378d6ffa446a043922d940c9ac37f22340
Filip Suk <filipsuk@gmail.com>
2016-03-06 18:31:51 +0100

    Sticky subscription box - screen bottom

--- www/js/main.js
+++ www/js/main.js
@@ -9,4 +9,25 @@ $(function () {
     // Move email subscription
     $('#subscription').appendTo('.subscription-box');
     $('.subscription-box input').attr('form', $('form').attr('id'));
+
+    // Sticky subscription box
+    // TODO: improve on mobile (now footer hidden on mobile to make it work)
+    $(window).on("scroll", function() {
+        var box = $('.subscription-box');
+        // Fixed above footer
+        if ($('footer')[0].getBoundingClientRect().top - $(window).height() < 0) {
+            box.removeClass('fixed')
+                .addClass('fixed-down');
+        }
+        // Fixed on bottom of the screen
+        else if ($('#main')[0].getBoundingClientRect().top  - $(window).height() < -420) {
+            box.addClass('fixed')
+                .removeClass('fixed-down');;
+        }
+        // In the middle of events list ready to be fixed
+        else {
+            box.removeClass('fixed');
+        }
+    });
+
 });
