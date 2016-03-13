cf474d386bbe52d43f7fd7856cd354a9e355348d
Jakub Zaba <kuba.zaba@gmail.com>
2016-03-13 22:46:27 +0100

    [Core] UserModel: added hashAndEncrypt method

--- app/modules/core/model/UserModel.php
+++ app/modules/core/model/UserModel.php
@@ -2,13 +2,27 @@
 
 namespace App\Modules\Core\Model;
 
-use App\Model\Exceptions\Subscription\EmailExistsException;
+use App\Modules\Front\Model\Exceptions\Subscription\EmailExistsException;
+use CannotPerformOperationException;
+use Nette\Database\Context;
 use Nette\Database\Table\IRow;
+use Nette\DI\Container;
+use Nette\Security\Passwords;
 
 
 class UserModel extends BaseModel
 {
 	const TABLE_NAME = 'users';
+
+	/** @var Container */
+	private $container;
+
+
+	public function __construct(Context $database, Container $container)
+	{
+		parent::__construct($database);
+		$this->container = $container;
+	}
 
 
 	/**
@@ -44,5 +58,18 @@ class UserModel extends BaseModel
 		return (bool)$this->getAll()
 			->where('email', $email)
 			->fetch();
+	}
+
+
+	/**
+	 * Hash and encrypt the password
+	 * @param string $password
+	 * @return string
+	 * @throws CannotPerformOperationException
+	 */
+	public function hashAndEncrypt($password)
+	{
+		$key = $this->container->getParameters()['security']['key'];
+		return base64_encode(\Crypto::Encrypt(Passwords::hash($password), $key));
 	}
 }
