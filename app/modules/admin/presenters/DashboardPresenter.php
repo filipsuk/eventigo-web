2f055be0c83d101489e7d092ad853ce4b42eaa45
Jakub Zaba <kuba.zaba@gmail.com>
2016-03-15 23:44:35 +0100

    Dashboard events info box

--- app/modules/admin/presenters/DashboardPresenter.php
+++ app/modules/admin/presenters/DashboardPresenter.php
@@ -2,7 +2,23 @@
 
 namespace App\Modules\Admin\Presenters;
 
+use App\Modules\Core\Model\EventModel;
+use Nette\Utils\DateTime;
+
 
 class DashboardPresenter extends BasePresenter
 {
+	/** @var EventModel @inject */
+	public $eventModel;
+
+
+	public function actionDefault()
+	{
+		$this->template->eventsCounts = $this->eventModel->getAll()
+			->select('COUNT(*) AS all')
+			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS thisMonth', $datetime = new DateTime)
+			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS nextMonth', $datetime->modifyClone('+1 MONTH'))
+			->where('start >= ?', $datetime)
+			->fetch();
+	}
 }
