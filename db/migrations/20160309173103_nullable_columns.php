ff8b4eac60c5a74ed93348aa4ef19ba52eca722e
Jakub Zaba <kuba.zaba@gmail.com>
2016-03-17 20:22:36 +0100

    Cleanup migration

--- db/migrations/20160309173103_nullable_columns.php
+++ db/migrations/20160309173103_nullable_columns.php
@@ -1,13 +1,11 @@
 <?php
 
-use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
 class NullableColumns extends AbstractMigration
 {
     public function change()
     {
-        //ALTER TABLE ygpnaptynr.events MODIFY image VARCHAR(2083) COMMENT 'url';
         $this->table('events')
             ->changeColumn('description', 'text', ['null' => true])
             ->changeColumn('end', 'datetime', ['null' => true])
