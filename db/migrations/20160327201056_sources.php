777058dedd8ec2d0ee8ed1617bf32f5530519e30
Jakub Zaba <kuba.zaba@gmail.com>
2016-04-05 08:36:10 +0200

    [admin] Added source model

---
+++ db/migrations/20160327201056_sources.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Sources extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('sources')
+            ->addColumn('name', 'string', ['limit' => 1024, 'null' => true])
+            ->addColumn('url', 'string', ['limit' => 2083, 'null' => true])
+            ->addColumn('organiser_id', 'integer', ['signed' => false, 'null' => true])
+            ->addColumn('event_series_id', 'integer', ['signed' => false, 'null' => true])
+            ->addColumn('check_frequency', 'integer', ['default' => 1])
+            ->addColumn('next_check', 'date')
+            ->addColumn('created', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+            ->addForeignKey('organiser_id', 'organisers', 'id', ['update' => 'CASCADE', 'delete' => 'CASCADE'])
+            ->addForeignKey('event_series_id', 'events_series', 'id', ['update' => 'CASCADE', 'delete' => 'CASCADE'])
+            ->create();
+    }
+}
