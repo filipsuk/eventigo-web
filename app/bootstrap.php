806ea2cdebad83c720228fb248b51a5097c90735
Filip Suk <filipsuk@gmail.com>
2016-04-18 19:53:04 +0200

    Fix redirects

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,6 +7,12 @@ $configurator = new Nette\Configurator;
 if (PHP_SAPI !== "cli") {
 	$configurator->setDebugMode(in_array($_SERVER['HTTP_HOST'], ['kuba.eventigo.cz', 'filip.eventigo.cz', 'eventigo.local', 'eventigo.local.cz']) || $_GET['ladenka'] === '1');
 }
+
+// Fix redirects to port 80 (https://forum.nette.org/cs/13896-openshift-redirect-z-https-pridava-port-80)
+if ( !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ) {
+	$_SERVER['SERVER_PORT'] = 443;
+}
+
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
