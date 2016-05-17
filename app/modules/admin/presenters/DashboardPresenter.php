2de488518c893dc1f60c38e02ccab257b0b6de85
Jakub Zaba <kuba.zaba@gmail.com>
2016-05-17 09:03:32 +0200

    [admin] Dashboard - not approved events info-box

--- app/modules/admin/presenters/DashboardPresenter.php
+++ app/modules/admin/presenters/DashboardPresenter.php
@@ -14,12 +14,20 @@ class DashboardPresenter extends BasePresenter
 
 	public function actionDefault()
 	{
-		$this->template->eventsCounts = $this->eventModel->getAll()
+		$this->template->approvedEventsCounts = $this->eventModel->getAll()
 			->select('COUNT(*) AS all')
 			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS thisMonth', $datetime = new DateTime)
 			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS nextMonth', $datetime->modifyClone('+1 MONTH'))
 			->where('start >= ?', $datetime)
 			->where('state = ?', EventModel::STATE_APPROVED)
+			->fetch();
+
+		$this->template->notApprovedEventsCounts = $this->eventModel->getAll()
+			->select('COUNT(*) AS all')
+			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS thisMonth', $datetime = new DateTime)
+			->select('COUNT(IF(MONTH(start) = MONTH(?), 1, NULL)) AS nextMonth', $datetime->modifyClone('+1 MONTH'))
+			->where('start >= ?', $datetime)
+			->where('state = ?', EventModel::STATE_NOT_APPROVED)
 			->fetch();
 	}
 }
