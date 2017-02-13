f9190d9568301731db33c856b27519fd86b054a9
TomasVotruba <tomas.vot@gmail.com>
2017-02-14 00:43:32 +0100

    drop robot-loader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -17,13 +17,7 @@ $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
-
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-$container = $configurator->createContainer();
-
-return $container;
+return $configurator->createContainer();
