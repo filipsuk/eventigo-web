3e6b27a217a3317c1a9ce67004881bf788fd02e9
TomasVotruba <tomas.vot@gmail.com>
2017-02-14 00:35:42 +0100

    fix Class names to PSR-4

---
+++ app/modules/newsletter/model/NoEventsFoundException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filipsuk
+ * Date: 01.05.16
+ * Time: 21:10
+ */
+
+namespace App\Modules\Newsletter\Model;
+
+
+class Exception extends \Exception
+{
+
+}
+class NoEventsFoundException extends Exception
+{
+	
+}
