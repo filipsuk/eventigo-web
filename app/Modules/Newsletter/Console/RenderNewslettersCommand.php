9c73859a8dc0e28ec78a80ce4a1bbb66bea4a39f
TomasVotruba <tomas.vot@gmail.com>
2017-05-24 10:31:18 +0200

    [Newsletter] drop recatching exception, allready handled by Symfony\Console\Application

--- app/Modules/Newsletter/Console/RenderNewslettersCommand.php
+++ app/Modules/Newsletter/Console/RenderNewslettersCommand.php
@@ -4,7 +4,6 @@ namespace App\Modules\Newsletter\Console;
 
 use App\Modules\Core\Model\UserModel;
 use App\Modules\Newsletter\Model\NewsletterService;
-use App\Modules\Newsletter\Model\NoEventsFoundException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,21 +29,21 @@ final class RenderNewslettersCommand extends Command
 
     protected function configure(): void
     {
-        $this->setName('newsletters:render')
-            ->setDescription('Render users newsletters prepared to send');
+        $this->setName('newsletters:render');
+        $this->setDescription('Render users newsletters prepared to send');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $users = $this->userModel->getAll()->where('newsletter', true)->fetchAll();
+        $users = $this->userModel->getAll()
+            ->where('newsletter', true)
+            ->fetchAll();
+
         $createdCount = 0;
+
         foreach ($users as $user) {
-            try {
-                $this->newsletterService->createUserNewsletter($user->getPrimary());
-                ++$createdCount;
-            } catch (NoEventsFoundException $e) {
-                $output->writeln($e->getMessage());
-            }
+            $this->newsletterService->createUserNewsletter($user->getPrimary());
+            ++$createdCount;
         }
 
         $output->writeln($createdCount . ' newsletters have been created');
